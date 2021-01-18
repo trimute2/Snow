a5a5c536d75aaebc7a75ae4a9ba453ee0fac82ef
Hanrui Zhang <71097187+Henry9708@users.noreply.github.com>
2021-01-18 07:46:31 -0500

    Add test scene for main features

---
+++ Snow Game/Assets/Scripts/Background.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+    private Vector2 offset;
+    float nowpos;
+    void Start()
+    {
+        offset = new Vector2(0, 0);
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowpos += speed * Time.deltaTime;
+        offset = new Vector2(0f, nowpos);
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ Snow Game/Assets/Scripts/Moving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public float speed;
+
+    public Transform MovePosition;
+
+    private Rigidbody Rig;
+    private float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Rig.velocity = new Vector3(speed * horizontal, Rig.velocity.y, Rig.velocity.z);
+    }
+}
