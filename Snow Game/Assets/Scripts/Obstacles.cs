5822d33f2d619d324258c35e41ddb7d0aeaff1ac
Hanrui Zhang <71097187+Henry9708@users.noreply.github.com>
2021-01-19 15:18:32 -0500

    Add obstacles script

---
+++ Snow Game/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameObject obstacles;
+    private Transform offset;
+    float nowpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obstacles.transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+    }
+}
