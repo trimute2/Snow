e8791fb75d3a0dde670ce638c0e3d70fcec6293b
Nuha Syed <nqs3694@rit.edu>
2021-01-19 00:54:04 -0500

    Implemented Main Menu (Play and Quit Buttons), Score Screen(Again Btn), Added Script to Change Between Scenes

---
+++ Snow Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    //Main Menu
+
+    const int numOfLevels = 2;
+    public void PlayGame()
+    {
+        if (SceneManager.GetActiveScene().buildIndex <= (numOfLevels-1))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            //go back to main menu if no screens left
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+
+    //quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
